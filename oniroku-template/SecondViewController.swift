31d826a9fd0f69d433cadede41ec30171293d60a
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2016-10-16 14:22:52 +0700

    Initial Commit

---
+++ oniroku-template/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  oniroku-template
+//
+//  Created by Takaaki Mizuno on 2016/10/16.
+//  Copyright © 2016年 Takaaki Mizuno. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
